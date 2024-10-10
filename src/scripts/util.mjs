bebaada9a9de0cc374d34c8dab88b72eef8cca22
Keii -けい- <keita19981209onepiece@gmail.com>
2024-10-10 21:56:45 +0900

    fix

--- src/scripts/util.mjs
+++ src/scripts/util.mjs
@@ -1,10 +1,17 @@
 import { MongoClient } from 'mongodb';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 
-// MongoDB の接続文字列を設定します
-const uri =
-  'mongodb+srv://keibo:ach0WcM36mBdLsF6@devlog.id9h6ei.mongodb.net/devlog?retryWrites=true&w=majority&appName=DevLog';
+import dotenv from 'dotenv';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: resolve(__dirname, '../../.env') });
 export async function connectDB() {
+  // MongoDB の接続文字列を設定します
+  const uri = process.env.DB_URI;
+
   const client = new MongoClient(uri);
   await client.connect();
   const database = client.db('devlog');
